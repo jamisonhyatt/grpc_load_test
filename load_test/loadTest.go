83eabb51544d7a2e933634bb09545945d4ba18f5
Jamison Hyatt <jamison.hyatt@capitalone.com>
2017-02-05 22:35:29 -0800

    initial commit

---
+++ load_test/loadTest.go
@@ -0,0 +1,118 @@
+package load_test
+
+import (
+	"time"
+	"sync/atomic"
+	"math/rand"
+	"google.golang.org/grpc"
+	"context"
+	"sync"
+	"strconv"
+	"io"
+	"fmt"
+)
+
+type LoadTestRequest struct {
+	Hostname string
+	Port int
+	TestLength time.Duration
+	ConcurrentRequests int
+	LoopsPerRequest int
+}
+
+type ExecutionResults struct {
+	TotalExecutions      uint64
+	SuccessfulExecutions uint64
+	ErrorExecutions    uint64
+	StreamedMessages     uint64
+}
+
+var executionResults = ExecutionResults{}
+func Loadtest(test LoadTestRequest) ExecutionResults {
+
+	rand.Seed(time.Now().Unix())
+	endLoadTest := time.Now().Add(test.TestLength)
+
+	conn, err := grpc.Dial(test.Hostname+":"+strconv.Itoa(test.Port), grpc.WithInsecure())
+
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	go printElapsed(time.Now(), endLoadTest)
+	for time.Now().Before(endLoadTest) {
+		paralellLoadTest(test.ConcurrentRequests, test.LoopsPerRequest, conn)
+	}
+
+	return executionResults
+}
+
+func printElapsed(start, end time.Time) {
+	for time.Now().Before(end) {
+		time.Sleep(time.Minute)
+		fmt.Printf("%v minute(s) elapsed...\n", int(time.Since(start).Minutes()))
+	}
+}
+
+func paralellLoadTest(numGoRoutines, numLoopsPerRoutine int, conn *grpc.ClientConn ) {
+	var wg sync.WaitGroup
+	//start := time.Now()
+	wg.Add(numGoRoutines)
+	for i := 1; i <= numGoRoutines; i++ {
+		go asyncClientExecution(&wg, conn, numLoopsPerRoutine)
+	}
+	wg.Wait()
+}
+
+func asyncClientExecution ( wg *sync.WaitGroup, conn *grpc.ClientConn, executionsPer int  ) {
+	executeClient(executionsPer, conn)
+	wg.Done()
+
+}
+
+
+func executeClient(numCalls int, conn *grpc.ClientConn  ) {
+	for i := 1; i <= numCalls; i ++ {
+		listFeatures(conn)
+		atomic.AddUint64(&executionResults.TotalExecutions, 1)
+	}
+
+}
+
+
+func listFeatures( conn *grpc.ClientConn ) {
+
+	routeGuideClient := NewRouteGuideClient(conn)
+
+	stream, err := routeGuideClient.ListFeatures(context.Background(), &Rectangle{
+		Hi: &Point{
+			Longitude: 800000000,
+			Latitude: 800000000,
+		},
+		Lo: &Point{
+			Longitude: -800000000,
+			Latitude: -800000000,
+		},
+
+
+	})
+
+	if err != nil {
+		atomic.AddUint64(&executionResults.ErrorExecutions, 1)
+		return
+	}
+	streamedCount := 0
+	for {
+		_, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			atomic.AddUint64(&executionResults.ErrorExecutions, 1)
+			return
+		}
+		streamedCount++
+	}
+	atomic.AddUint64(&executionResults.StreamedMessages, uint64(streamedCount))
+	atomic.AddUint64(&executionResults.SuccessfulExecutions, 1)
+}
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import "flag"
+import (
+	"./load_test"
+	"time"
+	"fmt"
+	"runtime"
+	"encoding/json"
+	"os"
+)
+
+func main() {
+	hostName := flag.String("h", "localhost", "the hostname")
+	port := flag.Int("p", 8980, "The port")
+	minutes := flag.Int("l", 1, "the length in minutes to run the test for")
+	concurrentRequests := flag.Int("c", 500, "the number of concurrent requests")
+	loopsPerReqeust := flag.Int("lpr", 5, "the number of times " +
+		"each concurrent request should loop")
+	flag.Parse()
+	if flag.NFlag() == 0 {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+
+	testLength := time.Minute * time.Duration(*minutes)
+	testRequest := load_test.LoadTestRequest{
+		Hostname: *hostName,
+		Port: *port,
+		TestLength: testLength,
+		ConcurrentRequests: *concurrentRequests,
+		LoopsPerRequest: *loopsPerReqeust,
+	}
+	requestJson, err := json.MarshalIndent(&testRequest,"", "  ")
+	if err != nil {
+		fmt.Errorf("Error parsing json: %v", err)
+		os.Exit(1)
+	}
+	fmt.Println("Test Details: \n", string(requestJson))
+	fmt.Printf("Timed test.  Will execute test for %v with the below settings\n", testLength)
+	fmt.Printf("%v concurrent requests with %v grpc executions per concurrent request " +
+		"on %v logical processors\n",*concurrentRequests, *loopsPerReqeust, runtime.NumCPU())
+	fmt.Println("Started...")
+
+	start := time.Now()
+	results := load_test.Loadtest(testRequest)
+	duration := time.Since(start)
+	fmt.Println("...Complete.")
+
+	fmt.Printf("\nTotal Test Time took %v\nStarted at %v, ended at %v.\n",time.Since(start), start, time.Now())
+	fmt.Printf("Completed %v grpc calls in %v.\n%v requests per second.\n",
+		results.TotalExecutions, duration, float64(results.TotalExecutions) / duration.Seconds())
+	fmt.Printf("%v successful, %v errored, \n", results.SuccessfulExecutions, results.ErrorExecutions)
+	fmt.Printf("%v individual streamed messages", results.StreamedMessages)
+}
